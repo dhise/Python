aad33e9228547b4abebbcfc12a5bd159d047b945
Jared Hise <jdhise@gmail.com>
2023-07-19 17:48:51 -0400

    Update email_sender.py
    Basic email sender to build from

--- email_sender.py
+++ email_sender.py
@@ -9,10 +9,26 @@ email['from'] = 'Jared Hise'
 email['to'] = 'jdhise@gmail.com'
 email['subject'] = 'Test email successful!'
 
-email.set_content(html.substitute(name='Sjplummer'), 'html')
+email.set_content(html.substitute(name='user_name'), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
     smtp.login('jurt.python@gmail.com', 'ykvqarbykfmymkns')
     smtp.send_message(email)
+
+
+
+############################################################################
+"""Seperate file within directory as index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    Hello $name your books are at your library.
+</body>
+</html>"""
+############################################################################
